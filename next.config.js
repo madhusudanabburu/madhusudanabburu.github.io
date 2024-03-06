9d648624974fe8e9f393ac14566d76bbb5f5c906
A A <acquire@tejansystems.com>
2024-03-06 16:26:40 -0600

    Changed package.json and configured images as unoptimized during export

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
-images.unoptimized = true 
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+}
 module.exports = withNextra()
