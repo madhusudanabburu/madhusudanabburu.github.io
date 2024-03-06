e8209b7689a5b78ca1894820b531433015f43da2
A A <acquire@tejansystems.com>
2024-03-06 16:37:09 -0600

    Configured images as unoptimized during export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-
-import Image from 'next/image'
- 
-const UnoptimizedImage = (props) => {
-  return <Image {...props} unoptimized />
-}
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = {withNextra, UnoptimizedImage}
+module.exports = {withNextra, images: {
+  unoptimized: true,
+}}
