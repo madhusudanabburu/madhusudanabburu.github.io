52b9618eb4efc3107d708bfc2539d0a8e19dfc35
A A <acquire@tejansystems.com>
2024-03-07 15:12:48 -0600

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 const withMarkdoc = require('@markdoc/next.js');
 
 module.exports =
-  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+  withMarkdoc({schemaPath: './markdoc', mode: 'static'})({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   });
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+
+  assetPrefix: ".",
+
+  basePath: '/out',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+/*module.exports = nextConfig*/
