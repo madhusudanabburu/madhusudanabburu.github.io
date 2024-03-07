c1855fc893b290cf00d66649949334d4cc48cddb
A A <acquire@tejansystems.com>
2024-03-06 21:19:46 -0600

    Changed next.config.js to export static output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra();
+module.exports = nextConfig;
